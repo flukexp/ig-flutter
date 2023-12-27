cce8a4a25c734a15d89a79959910d02da535e9ae
flukexp <flukelolza@gmail.com>
2023-12-27 15:25:29 +0700

    IOS and Firebase Setup

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:art_directory/responsive/mobile_screen_layout.dart';
+import 'package:art_directory/responsive/responsive_layout_screen.dart';
+import 'package:art_directory/responsive/web_screen_layout.dart';
+import 'package:art_directory/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Art Directory',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const ResponsiveLayout(mobileScreenLayout: MobileScreenLayout(),webScreenLayout: WebScreenLayout(),),
+    );
+  }
+}
+
