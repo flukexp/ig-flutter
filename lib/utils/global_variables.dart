84977af5b22fe6d938b9afee0866d6d93aabda9b
flukexp <flukelolza@gmail.com>
2024-01-02 00:37:30 +0700

    Seach section

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:art_directory/screens/add_post_screen.dart';
 import 'package:art_directory/screens/feed_screen.dart';
+import 'package:art_directory/screens/search_screen.dart';
 import 'package:flutter/material.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('favorite'),
   Text('person'),
